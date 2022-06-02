2ce1d0effb9d4605d4d806301cedd4e26e51a959
Frank Andreas <andreass.frankk@gmail.com>
2022-06-02 16:38:45 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Blockchain Architecture</title>
+        
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Blockchain Architecture's website!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          We will release the website soon.
         </p>
       </main>
 
